29170e103bb2c8f662a00b32f2ff8614df37e39e
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-01-18 16:52:23 +0530

    login component made

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import "./App.css";
+import Login from "./components/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
 function App() {
   return (
-    <div>
-      <h2>Let's build the linked clone</h2>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route
+            exact
+            path="/home"
+            element={
+              <>
+                <Header />
+                <Home />
+              </>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header = () => {
+  return (
+    <Container>
+      <Content>header</Content>
+    </Container>
+  );
+};
+const Container = styled.div``;
+const Content = styled.div``;
+export default Header;
